fe2755bc682529954c32956df0b1c53360e0b43f
NickZeroCode <faraycode@gmail.com>
2025-03-17 11:38:46 +0100

    db_name

--- systeme_monitoring/settings.py
+++ systeme_monitoring/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'systeme_monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(tempfile.gettempdir(), 'db.sqlite3'),
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
